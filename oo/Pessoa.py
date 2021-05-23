d61a32cd4664e4d77be676353aba01e2165e839e
ChamaDev <charles.sts@gmail.com>
2021-05-23 08:50:37 -0300

    Criado e removido atributo dinamico do tipo pessoa

--- oo/Pessoa.py
+++ oo/Pessoa.py
@@ -18,3 +18,7 @@ if __name__ == '__main__':
     print(luciano.idade)
     for filhos in luciano.filhos:
         print(filhos.nome)
+    luciano.sobrenome = 'Ramalho'
+    print(luciano.__dict__)
+    print(renzo.__dict__)
+    del luciano.filhos
