8dd30181a23dc0c7a14fb838540665c2fc9dc506
ChamaDev <charles.sts@gmail.com>
2021-05-23 07:46:44 -0300

    Criado atributos de instância nome e idade

--- oo/Pessoa.py
+++ oo/Pessoa.py
@@ -1,2 +1,16 @@
 class Pessoa:
-    pass
+    def __init__(self, nome=None, idade=44):
+        self.nome = nome
+        self.idade = idade
+
+    def cumprimentar(self):
+        return f'Olá {id(self)}'
+
+
+if __name__ == '__main__':
+    p = Pessoa('Charles')  # Cria instancia/objeto
+    print(Pessoa.cumprimentar(p))
+    print(id(p))
+    print(p.cumprimentar())
+    print(p.nome)
+    print(p.idade)
